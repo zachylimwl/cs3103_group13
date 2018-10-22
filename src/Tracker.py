eb156e1a46fd49a4709674c1b1c0bf829725a65c
peng229 <e0003632@u.nus.edu>
2018-10-22 21:50:37 +0800

    Add debugging print statements in tracker

--- src/Tracker.py
+++ src/Tracker.py
@@ -70,9 +70,14 @@ class Tracker:
                         self.lock.acquire()
                         peer_id = self.handle_acquire_message(payload)
                         self.lock.release()
+                        #debug...
+                        #print(self.file_details)
+                        #print(self.file_owners)
+                        #print(self.chunk_details)
+
                         #if tcp need to ack back ??? then ack using the peer_id (source ip and port all there)
                     #create if statements for other types of messages here
-
+                    #if payload[MESSAGE_TYPE] == other request type:
             except Exception as e:
                 client.close()
                 print(e)
