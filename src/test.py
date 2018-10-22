fdc84591d1cca5c6ad0ea2eb35a1db86d33ec035
peng229 <e0003632@u.nus.edu>
2018-10-22 21:50:52 +0800

    just PC's test py file

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import string
 import hashlib
 os.chdir('../test_directory')
 dir = os.getcwd()
-print(dir)
+#print(dir)
 # for f in os.walk(dir):
 #     for filename in f[2]:
 #         print(os.path.join(dir, filename))
@@ -20,15 +20,20 @@ res = {}
 full_path = os.path.join(dir, file_name)
 checksum = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
 file_size = os.stat(full_path).st_size
-print("size of file: " + str(file_size))
+#print("size of file: " + str(file_size))
 leftover_bytes = file_size % CHUNK_SIZE
 if leftover_bytes == 0:
     number_of_chunks = int(file_size / CHUNK_SIZE)
 else:
     number_of_chunks = int((file_size / CHUNK_SIZE) + 1)
 
-print(number_of_chunks)
+#print(number_of_chunks)
 res["checksum"] = checksum
 res["number_of_chunks"] = number_of_chunks
 res["file_name"] = file_name
-print(res)
+#print(res)
+
+ch = "file_teset_1.chunk"
+string = ch.split(".")
+arr = string[0].rsplit('_', 1)
+print(arr)
